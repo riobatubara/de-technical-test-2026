6edcb1b5869a884227b980629355f08f4fdee70f
riobatubara <outrageryo@gmail.com>
2026-08-04 07:40:01 +0700

    chore: create check on products.price, transactions.total_amount, transaction_items.price and marketing_campaings.end_date

--- database/01_schema.sql
+++ database/01_schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE customers (
+CREATE TABLE IF NOT EXISTS customers (
     customer_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(150) UNIQUE NOT NULL,
@@ -6,21 +6,21 @@ CREATE TABLE customers (
     signup_date DATE NOT NULL
 );
 
-CREATE TABLE products (
+CREATE TABLE IF NOT EXISTS products (
     product_id SERIAL PRIMARY KEY,
     product_name VARCHAR(150) NOT NULL,
     category VARCHAR(100) NOT NULL,
     price DECIMAL(10, 2) NOT NULL CHECK (price >= 0)
 );
 
-CREATE TABLE transactions (
+CREATE TABLE IF NOT EXISTS transactions (
     transaction_id SERIAL PRIMARY KEY,
     customer_id INT NOT NULL REFERENCES customers(customer_id) ON DELETE CASCADE,
     transaction_date DATE NOT NULL,
     total_amount DECIMAL(12, 2) NOT NULL CHECK (total_amount >= 0)
 );
 
-CREATE TABLE transaction_items (
+CREATE TABLE IF NOT EXISTS transaction_items (
     transaction_item_id SERIAL PRIMARY KEY,
     transaction_id INT NOT NULL REFERENCES transactions(transaction_id) ON DELETE CASCADE,
     product_id INT NOT NULL REFERENCES products(product_id) ON DELETE CASCADE,
@@ -28,7 +28,7 @@ CREATE TABLE transaction_items (
     price DECIMAL(10, 2) NOT NULL CHECK (price >= 0)
 );
 
-CREATE TABLE marketing_campaigns (
+CREATE TABLE IF NOT EXISTS marketing_campaigns (
     campaign_id SERIAL PRIMARY KEY,
     campaign_name VARCHAR(150) NOT NULL,
     start_date DATE NOT NULL,
