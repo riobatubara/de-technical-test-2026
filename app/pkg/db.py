ec819bd1def324ad65f26f42d8e096a61dc78ef4
riobatubara <outrageryo@gmail.com>
2026-08-06 08:26:45 +0700

    feat: add db connection

---
+++ app/pkg/db.py
@@ -0,0 +1,24 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from app.pkg.config import conf
+
+
+def get_connection():
+    return psycopg2.connect(
+        host=conf.DB_HOST,
+        dbname=conf.DB_NAME,
+        user=conf.DB_USER,
+        password=conf.DB_PASSWORD,
+        port=conf.DB_PORT,
+    )
+
+
+def get_cursor(conn):
+    return conn.cursor(cursor_factory=RealDictCursor)
+
+
+def close(conn=None, cursor=None):
+    if cursor:
+        cursor.close()
+    if conn:
+        conn.close()
