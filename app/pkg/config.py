39f775f2ff7837aa9da6220cb9aeeeb14f782d32
riobatubara <outrageryo@gmail.com>
2026-08-06 08:25:38 +0700

    feat: config module to parse env

---
+++ app/pkg/config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    DB_HOST = os.getenv('DB_HOST', 'postgres')
+    DB_NAME = os.getenv('DB_NAME', 'tech_test_db')
+    DB_USER = os.getenv('DB_USER', 'user')
+    DB_PASSWORD = os.getenv('DB_PASSWORD', 'secret')
+    DB_PORT = os.getenv('DB_PORT', '5432')
+
+    KAFKA_BROKERS = os.getenv("KAFKA_BROKERS", "localhost:9092")
+    KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "stream.data.raw")
+    KAFKA_GROUP = os.getenv("KAFKA_GROUP", "data_stream_group")
+
+conf = Config()
